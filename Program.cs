7352e5c87cc71f366967021245b5f47fbd7d4c5b
ElizabethCowell <80732868+ElizabethCowell@users.noreply.github.com>
2021-04-05 12:15:01 -0400

    Starting adventure game

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TinyAdventure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Oh Honey, You Tiny!");
+            Console.ReadKey();
+
+        }
+    }
+}
